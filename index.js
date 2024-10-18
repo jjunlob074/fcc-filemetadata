dc6c6d789c1c836b94bc91d694c00d11c9dec2b0
jjunlob074 <jjunlob074@g.educaand.es>
2024-10-18 02:13:22 +0200

    SOLUCION CON MULTER BY JJUNLOB074

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
 
 var app = express();
+
+// Configura Multer
+var storage = multer.memoryStorage(); // Almacena el archivo en memoria
+var upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +16,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Endpoint para manejar la carga de archivos
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  // Verifica si se ha subido un archivo
+  if (!req.file) {
+    return res.status(400).send({ error: 'No file uploaded' });
+  }
+  
+  // Prepara la respuesta
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
 
-
+  res.json(fileInfo); // Devuelve la información del archivo como JSON
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
